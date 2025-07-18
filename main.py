55fd7024671918d6dd379d38580682b72aefa8d2
Neil Davies <neil.davies92@gmail.com>
2025-07-18 14:16:23 +0100

    refactor to only use socket package

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import socket
-from http.server import HTTPServer, BaseHTTPRequestHandler
 
 hostname = 'localhost'
-port = 8080
+port = 9000
 
-def run_server(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = (hostname, port)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
-
-#TODO: Create a socket, and bind it to the address of your server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((hostname, port))
+s.listen(1)
 
-#TODO: Listen for incoming connections
-#TODO: Accept incoming requests
-#TODO: Recieve incoming data
-#TODO: Close the connection
-# run_server()
+conn, addr = s.accept()
+print(f"Connection from {addr}")
+
+conn.send(b"Hello, world!")
+conn.close()
+s.close()
