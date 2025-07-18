219f0ca13349b72af5c755b58359690227dd9bd2
Neil Davies <neil.davies92@gmail.com>
2025-07-18 21:43:39 +0100

    return 400 if invalid file is parsed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import socket
 
 hostname = 'localhost'
 port = 80
+file = 'index.html'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((hostname, port))
@@ -19,16 +20,21 @@ if request_lines:
     method, path, version = request_line.split()
 
 if path == '/':
-    path = f'/index.html'
+    path = f'/{file}'
 
-with open('www/index.html', 'r') as file:
-    body = file.read()
-
-response = (
-    f"{version} 200 OK\r\n\n"
-    f"{body}"
-)
-print(response)
+if path == f'/{file}':
+    with open('www/index.html', 'r') as f:
+        body = f.read()
+    response = (
+        f"{version} 200 OK\r\n\r\n"
+        f"{body}"
+    )
+else:
+    body = "<h1>400 Bad Request</h1>"
+    response = (
+        f"{version} 400 Bad Request\r\n\r\n"
+        f"{body}"
+    )
 
 conn.sendall(response.encode('utf-8'))
 s.close()
