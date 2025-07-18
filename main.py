b9d57118f307db2dfd9201e194149e3d365da788
Neil Davies <neil.davies92@gmail.com>
2025-07-18 17:44:05 +0100

    parse request and send response over socket

--- main.py
+++ main.py
@@ -8,8 +8,21 @@ s.bind((hostname, port))
 s.listen(1)
 
 conn, addr = s.accept()
-print(f"Connection from {addr}")
+print(f"Connection from {addr}\n")
 
-conn.send(b"Hello, world!")
-conn.close()
+request = conn.recv(1024)
+request_text = request.decode('utf-8')
+print(request_text)
+
+request_lines = request_text.splitlines()
+if request_lines:
+    request_line = request_lines[0]
+    method, path, version = request_line.split()
+    print(f"Method: {method}, Path: {path}, Version: {version}")
+
+response = (
+    f"{version} 200 OK\r\n"
+    f"Requested path: {path}\r\n"
+)
+conn.sendall(response.encode('utf-8'))
 s.close()
