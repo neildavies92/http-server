1923a3ec50393b0c180d76be4c97a08fe68181dc
Neil Davies <neil.davies92@gmail.com>
2025-07-18 13:57:13 +0100

    create socket and bind to address

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import socket
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+hostname = 'localhost'
+port = 8080
+
 def run_server(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('localhost', 8080)
+    server_address = (hostname, port)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
-run_server()
+#TODO: Create a socket, and bind it to the address of your server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((hostname, port))
+
+#TODO: Listen for incoming connections
+#TODO: Accept incoming requests
+#TODO: Recieve incoming data
+#TODO: Close the connection
+# run_server()
