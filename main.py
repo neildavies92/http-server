cff93356310426775acd490b1a4b16a47b42e271
Neil Davies <neil.davies92@gmail.com>
2025-07-23 10:08:45 +0100

    add handle_request function

--- main.py
+++ main.py
@@ -1,43 +1,40 @@
 import socket
 
-hostname = "localhost"
-port = 8000
-file = "index.html"
+HOSTNAME = "localhost"
+PORT = 8000
+FILE = "index.html"
 
 
-def create_socket():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((hostname, port))
-        s.listen(1)
-        print(f"Listening on http://{hostname}:{port}...")
-        return s
-    except Exception as e:
-        print(f"Error creating socket: {e}")
-        return None
+def create_socket(host, port):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(1)
+    print(f"Listening on http://{host}:{port}...")
+    return server
 
 
-s = create_socket()
-if s is None:
-    exit(1)
-
-while True:
-    conn, addr = s.accept()
+def handle_request(server):
+    conn, addr = server.accept()
+    data = conn.recv(1024).decode("utf-8")
     print(f"Connection from {addr}\n")
 
-    request = conn.recv(1024).decode("utf-8")
-    print(request)
-
-    request_lines = request.splitlines()
+    request_lines = data.splitlines()
     if request_lines:
         request_line = request_lines[0]
         method, path, version = request_line.split()
 
-    if path in ("/", f"/{file}"):
-        with open("www/index.html", "r") as f:
+    if path in ("/", f"/{FILE}"):
+        with open(f"www/{FILE}", "r") as f:
             body = f.read()
-        response = f"{version} 200 OK\r\n\r\n" f"{body}"
+        response = f"{version} 200 OK\r\n\r\n{body}"
     else:
         response = f"{version} 404 File Not Found\r\n\r\n"
+
     conn.sendall(response.encode("utf-8"))
     conn.close()
+
+
+server = create_socket(HOSTNAME, PORT)
+
+while True:
+    handle_request(server)
