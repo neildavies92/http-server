1a7d2ab7be68bd247b960311917f79059817b4b0
Neil Davies <neil.davies92@gmail.com>
2025-07-23 08:24:10 +0100

    add create_socket function

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ hostname = "localhost"
 port = 8000
 file = "index.html"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((hostname, port))
-s.listen(1)
-print(f"Listening on http://{hostname}:{port}...")
+
+def create_socket():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((hostname, port))
+        s.listen(1)
+        print(f"Listening on http://{hostname}:{port}...")
+        return s
+    except Exception as e:
+        print(f"Error creating socket: {e}")
+        return None
+
+
+s = create_socket()
+if s is None:
+    exit(1)
 
 while True:
     conn, addr = s.accept()
@@ -27,6 +39,5 @@ while True:
         response = f"{version} 200 OK\r\n\r\n" f"{body}"
     else:
         response = f"{version} 404 File Not Found\r\n\r\n"
-
     conn.sendall(response.encode("utf-8"))
     conn.close()
